b96f9b9498b2260b9a8617965302f39d5ce395ca
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-09-28 15:05:54 -0300

    modification for pg UIkit

---
+++ Login app/SecondScreen.swift
@@ -0,0 +1,19 @@
+//
+//  SecondScreenViewController.swift
+//  Login app
+//
+//  Created by Felipe Vieira on 28/09/23.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemMint
+
+    }
+
+}
