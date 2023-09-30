8455c40791198f4638cf991bbe8d779a30cf72c9
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-09-30 12:06:21 -0300

    add file sing in view controller

---
+++ Login app/SingInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingInViewController.swift
+//  Login app
+//
+//  Created by Felipe Vieira on 30/09/23.
+//
+
+import UIKit
+
+class SingInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+}
