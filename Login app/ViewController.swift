d3e85fca388bbdcb98fbb33c8ecc4e73e4b51b50
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-09-30 12:01:29 -0300

    remove func setupButton

--- Login app/ViewController.swift
+++ Login app/ViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let nextButton = UIButton()
-    
     let painelImage = UIImageView()
     
     let mainText = UILabel()
@@ -144,19 +142,6 @@ class ViewController: UIViewController {
         ])
     }
     
-    
-    func setupButton() {
-        view.addSubview(nextButton)
-        nextButton.configuration = .filled()
-        nextButton.configuration?.baseBackgroundColor = .systemPink
-        nextButton.configuration?.title = "Next"
-        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor), nextButton.widthAnchor.constraint(equalToConstant: 200), nextButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
-    }
     
     @objc func swipeHandler(_ gestureRecognizer : UISwipeGestureRecognizer) {
         if gestureRecognizer.direction == UISwipeGestureRecognizer.Direction.left {
