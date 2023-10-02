e880e7a1cfe3a9f710d8596f57b587bca7eaa783
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-10-02 12:31:36 -0300

    setup image and email text field

--- Login app/LoginViewController.swift
+++ Login app/LoginViewController.swift
@@ -11,9 +11,49 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.hidesBackButton = true
+        setupImage(painelImage)
+        setupEmailTextFild(emailTextField)
+        setupPassTF(passTextField)
+        setupButtonUnhidePass(buttonUnhide)
+    }
+    
+    
+    func setupImage(_ painelImage: UIImageView) {
+        painelImage.image = UIImage(resource: .painelLogin)
         
-        view.backgroundColor = .systemMint
+        painelImage.translatesAutoresizingMaskIntoConstraints = false
         
+        view.addSubview(painelImage)
+        
+        NSLayoutConstraint.activate([
+            painelImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 130),
+            painelImage.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            painelImage.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            painelImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    func setupEmailTextFild(_ textField: UITextField) {
+        textField.placeholder = "Email"
+        textField.leftView = UIView.init(frame: CGRect(x: 0, y: 0, width: 20, height: 0))
+        textField.leftViewMode = .always
+        textField.backgroundColor = .white
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.backgroundSegmented.cgColor
+        textField.layer.cornerRadius = 10
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: painelImage.bottomAnchor, constant: 50),
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.widthAnchor.constraint(equalToConstant: 350),
+            textField.heightAnchor.constraint(equalToConstant: 52),
+            
+        ])
     }
 
 }
