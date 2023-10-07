6f854238a457118e11bfa918b713f08db7e0d920
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-10-07 20:01:13 -0300

    add atribute image and texts

--- Login app/SingInCompletedView.swift
+++ Login app/SingInCompletedView.swift
@@ -8,6 +8,45 @@
 import UIKit
 
 class SingInCompletedView: UIViewController {
+    
+    // painel image sing in completed
+    private let painelImageView: UIImageView = {
+        let pi = UIImageView()
+        
+        pi.image = UIImage(resource: .painelThanksForSingIn)
+        
+        pi.translatesAutoresizingMaskIntoConstraints = false
+        return pi
+    }()
+    
+    
+    private let titlePainel: UILabel = {
+        let tp = UILabel()
+        
+        tp.text = "Happy! You have successfully registered"
+        
+        tp.font = .systemFont(ofSize: 21, weight: .bold)
+        tp.textAlignment = .center
+        tp.textColor = .black
+        tp.numberOfLines = 0
+        
+        tp.translatesAutoresizingMaskIntoConstraints = false
+        return tp
+    }()
+    
+    private let subTitlePainel: UILabel = {
+        let stp = UILabel()
+        
+        stp.text = "Please check your email to do so Verify before logging in."
+        
+        stp.font = .systemFont(ofSize: 15, weight: .light)
+        stp.textAlignment = .center
+        stp.textColor = .black
+        stp.numberOfLines = 0
+        
+        stp.translatesAutoresizingMaskIntoConstraints = false
+        return stp
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
