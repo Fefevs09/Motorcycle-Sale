034bfd07d83fcbab09baca35e73805e952641157
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-10-07 14:14:34 -0300

    add file sing in completed view

---
+++ Login app/SingInCompletedView.swift
@@ -0,0 +1,17 @@
+//
+//  SingInCompletedView.swift
+//  Login app
+//
+//  Created by Felipe Vieira on 07/10/23.
+//
+
+import UIKit
+
+class SingInCompletedView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
